bc871767ef8e558f7281258f30726ca9dfe8d227
Olga Traczyk <o.traczyk@student.uw.edu.pl>
2018-03-09 15:43:07 +0100

    [ruby] Hamming - evolved

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,18 +1,12 @@
 class Hamming
-    def self.compute(strand_A, strand_B)
-        if strand_A.length != strand_B.length
-            raise ArgumentError
-        end
-        diff_count = 0
-        (0..strand_A.length).each do |i|
-            if strand_A[i] != strand_B[i]
-                diff_count += 1
-            end
-        end
-        return diff_count
+  def self.compute(strand_A, strand_B)
+    raise ArgumentError if strand_A.length != strand_B.length
+    strand_A.chars.zip(strand_B.chars).count do |nuc_A, nuc_B|
+      nuc_A != nuc_B
     end
+  end
 end
 
 module BookKeeping
-    VERSION = 3
+  VERSION = 3
 end
